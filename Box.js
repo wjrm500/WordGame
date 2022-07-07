c77f1bd306b2d4d57983b6f16f6b75c0c7466843
William May <wjrm500@gmail.com>
2022-07-07 23:00:45 +0800

    Factoring out box event listener logic to Box.js

--- Box.js
+++ Box.js
@@ -1,3 +1,6 @@
+import { generateRandomLetter } from './randomLetter.js';
+import { players, activePlayerIndex, colorBoxes } from './game.js';
+
 export class Box {
     constructor(elem, x, y) {
         this.elem = elem;
@@ -5,6 +8,38 @@ export class Box {
         this.y = y;
         this.letter = null;
         this.player = null;
+        this.setLetter(generateRandomLetter());
+        let box = this;
+        this.elem.addEventListener('click', function() {
+            // debugger;
+            if (!document.getElementById('word-entry').disabled) {
+                alert('Enter a word!');
+                return;
+            }
+            let player = players[activePlayerIndex];
+            let addBox = false;
+            for (let playerBox of player.boxes) {
+                let sameBox = box.x == playerBox.x && box.y == playerBox.y;
+                if (!sameBox && Math.abs(playerBox.x - box.x) <= 1 && Math.abs(playerBox.y - box.y) <= 1) {
+                    addBox = true;
+                    break;
+                }
+            }
+            if (addBox) {
+                player.addBox(box);
+                box.setPlayer(player);
+                colorBoxes();
+                for (let elem of document.getElementsByClassName('inner-box')) {
+                    elem.classList.replace('active', 'inactive');
+                }
+                document.getElementById('prompt').innerHTML = 'Enter a word that can be formed by your letters';
+                document.getElementById('word-entry-container').classList.replace('inactive', 'active');
+                document.getElementById('word-entry').disabled = false;
+                document.getElementById('word-entry').focus();
+            } else {
+                alert('You can\'t go here!');
+            }
+        });
     }
 
     setLetter(letter) {
--- game.js
+++ game.js
@@ -1,10 +1,8 @@
 import { Box } from './Box.js';
 import { Player } from './Player.js';
 import { Grid } from './Grid.js';
-import { generateRandomLetter } from './randomLetter.js';
 
 let grid = new Grid(document.getElementById('grid'));
-let players, activePlayerIndex;
 
 function createGrid(dimension) {
     for (let i = 0; i < dimension; i++) {
@@ -17,45 +15,14 @@ function createGrid(dimension) {
             innerBox.classList.add('inner-box', 'active');
             outerBox.appendChild(innerBox);
             row.appendChild(outerBox);
-
             let box = new Box(innerBox, i, j);
-            box.elem.addEventListener('click', function() {
-                if (!document.getElementById('word-entry').disabled) {
-                    alert('Enter a word!');
-                    return;
-                }
-                let player = players[activePlayerIndex];
-                let addBox = false;
-                for (let playerBox of player.boxes) {
-                    let sameBox = box.x == playerBox.x && box.y == playerBox.y;
-                    if (!sameBox && Math.abs(playerBox.x - box.x) <= 1 && Math.abs(playerBox.y - box.y) <= 1) {
-                        addBox = true;
-                        break;
-                    }
-                }
-                if (addBox) {
-                    player.addBox(box);
-                    box.setPlayer(player);
-                    colorBoxes();
-                    for (let elem of document.getElementsByClassName('inner-box')) {
-                        elem.classList.replace('active', 'inactive');
-                    }
-                    document.getElementById('prompt').innerHTML = 'Enter a word that can be formed by your letters';
-                    document.getElementById('word-entry-container').classList.replace('inactive', 'active');
-                    document.getElementById('word-entry').disabled = false;
-                    document.getElementById('word-entry').focus();
-                } else {
-                    alert('You can\'t go here!');
-                }
-            });
-            box.setLetter(generateRandomLetter());
             grid.addBox(box);
         }
         grid.elem.appendChild(row);
     }
 }
 
-function colorBoxes() {
+export function colorBoxes() {
     for (let box of grid.boxes) {
         if (box.player != null) {
             box.elem.style.backgroundColor = box.player.color;
@@ -63,13 +30,6 @@ function colorBoxes() {
             box.elem.style.border = '1px solid black';
         }
     }
-}
-
-function createPlayers() {
-    let player1 = new Player('Player 1', grid.getBox(0, 0), 'red', document.getElementById('player-1-score'));
-    let player2 = new Player('Player 2', grid.getBox(dimension - 1, dimension - 1), 'blue', document.getElementById('player-2-score'));
-    players = [player1, player2];
-    activePlayerIndex = 0;
 }
 
 function changePlayer() {
@@ -88,8 +48,10 @@ function changePlayer() {
 
 let dimension = 8;
 createGrid(dimension);
-// createBoxes();
-createPlayers();
+let player1 = new Player('Player 1', grid.getBox(0, 0), 'red', document.getElementById('player-1-score'));
+let player2 = new Player('Player 2', grid.getBox(dimension - 1, dimension - 1), 'blue', document.getElementById('player-2-score'));
+export let players = [player1, player2];
+export let activePlayerIndex = 0;
 colorBoxes();
 document.getElementById('active-player').innerHTML = players[activePlayerIndex].name;
 document.getElementById('word-entry').addEventListener('keypress', function(evt) {
