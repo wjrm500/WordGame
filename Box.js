9ed4bdd283fb4be391fee260fc03abdd80975bf3
William May <wjrm500@gmail.com>
2022-07-14 00:19:07 +0800

    Fix to prevent player clicking existing box

--- Box.js
+++ Box.js
@@ -18,8 +18,7 @@ export class Box {
             let player = players[activePlayerIndex];
             let addBox = false;
             for (let playerBox of player.boxes) {
-                let sameBox = box.x == playerBox.x && box.y == playerBox.y;
-                if (!sameBox && Math.abs(playerBox.x - box.x) <= 1 && Math.abs(playerBox.y - box.y) <= 1) {
+                if (!player.boxes.includes(box) && Math.abs(playerBox.x - box.x) <= 1 && Math.abs(playerBox.y - box.y) <= 1) {
                     addBox = true;
                     break;
                 }
